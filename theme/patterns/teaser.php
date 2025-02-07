5ec9620da2f87f9cb4b5b87412a02f6577af2b8f
Jacob Wayne Smith <jacob.smith@automattic.com>
2025-02-06 16:15:58 -0800

    adding theme

---
+++ theme/patterns/teaser.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Title: Teaser
+ * Slug: block-demo/teaser
+ */
+?>
+<!-- wp:group {"templateLock":"contentOnly","metadata":{"name":"Teaser"},"className":"sun-teaser","style":{"spacing":{"blockGap":"var:preset|spacing|20"}},"layout":{"type":"default"}} -->
+<div class="wp-block-group sun-teaser"><!-- wp:group {"style":{"spacing":{"blockGap":"0"}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"left"}} -->
+<div class="wp-block-group"><!-- wp:image {"width":"147px","height":"98px","scale":"contain","lock":{"move":true,"remove":true}} -->
+<figure class="wp-block-image is-resized"><img alt="" style="object-fit:contain;width:147px;height:98px"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:group {"style":{"spacing":{"padding":{"left":"var:preset|spacing|20"}}},"layout":{"type":"default"}} -->
+<div class="wp-block-group" style="padding-left:var(--wp--preset--spacing--20)"><!-- wp:paragraph {"lock":{"move":true,"remove":true},"metadata":{"name":"Headline"},"fontSize":"small"} -->
+<p class="has-small-font-size">Headline</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:heading {"level":3,"lock":{"move":true,"remove":true},"metadata":{"name":"Subdeck"},"fontSize":"small"} -->
+<h3 class="wp-block-heading has-small-font-size">Subdeck</h3>
+<!-- /wp:heading --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+
+<!-- wp:paragraph {"lock":{"move":true,"remove":true},"metadata":{"name":"Tag"},"fontSize":"small"} -->
+<p class="has-small-font-size">Tag</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
